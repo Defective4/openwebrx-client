574d9274ab168d852319e724407deaabb0e81c02
Defective <def3ctive4@gmail.com>
2026-03-02 20:13:36 +0100

    Add a fixed "Empty" mode

--- src/main/java/io/github/defective4/sdr/owrxclient/model/ReceiverMode.java
+++ src/main/java/io/github/defective4/sdr/owrxclient/model/ReceiverMode.java
@@ -2,6 +2,9 @@ package io.github.defective4.sdr.owrxclient.model;
 
 public record ReceiverMode(String modulation, String name, Type type, String[] requirements, boolean squelch,
         Bandpass bandpass, String[] underlying, boolean secondaryFft) {
+    
+    public static final ReceiverMode EMPTY = new ReceiverMode("empty", "Empty", null, new String[0], false, null, null, false);
+    
     public static enum Type {
 
     }
